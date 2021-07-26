045cc280ea7c2295370404452d94f097935c5083
Ricardo Shikata <rshikata@apoioprodesp.sp.gov.br>
2021-07-26 20:26:14 -0300

    Acerto no pow

--- src/main/java/apirest/maven/cadpessoas/mapper/PersonMapper.java
+++ src/main/java/apirest/maven/cadpessoas/mapper/PersonMapper.java
@@ -8,7 +8,7 @@ import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface  PersonMapper {
-        PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
     @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
     Person toModel(PersonDTO personDTO);
